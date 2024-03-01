141011e19ca23863ed8cf30733443e262375acf8
sai <addanki.saipavan@gmail.com>
2024-03-01 20:46:57 +0530

    commiting-projectleader-Test2-spyobj

---
+++ Maven_Example4/src/test/java/com/nit/Test2/AppTest.java
@@ -0,0 +1,41 @@
+package com.nit.Test2;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import BankingService.CustomerRegistrationATBank;
+import BankingService.CutomerRegistrationInBankImpClass;
+import BankingService.LoginDao;
+
+public class AppTest {
+	private static LoginDao dao;
+	private static CustomerRegistrationATBank login;
+@BeforeAll
+public  static void setUp() {
+	dao=Mockito.spy(LoginDao.class);
+	login=new CutomerRegistrationInBankImpClass(dao);
+	System.out.println(dao+"\t"+login);
+	
+}
+@Test
+@DisplayName("Equals")
+public void testWithEquals() {
+	login.add("saipavan", "saipavan12");
+	Mockito.verify(dao,Mockito.times(1)).register("saipavan", "saipavan12");
+	login.add("pavan", "pavan12");
+	login.add("pavan", "pavan12");
+	Mockito.verify(dao,Mockito.times(2)).register("pavan", "pavan12");
+	login.add("king", "king12");
+	login.add("king", "king12");
+	login.add("king", "king12");
+	Mockito.verify(dao,Mockito.times(3)).register("king", "king12");	
+}
+@AfterAll
+public static void tearDown() {
+	login=null;
+	dao=null;
+}
+}
